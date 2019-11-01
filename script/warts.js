dc06f3606e873e57c38f5505b074f3ea86f1befc
Cleiton Cabral <55886803+cleitoncabral@users.noreply.github.com>
2019-10-31 22:26:23 -0300

    Add files via upload
    Fazendo alguns ajustes visuais e adicionando descrições das respectivas casas, além de seus brasões.

--- script/warts.js
+++ script/warts.js
@@ -1,10 +1,10 @@
-const $logo = document.querySelectorAll('.logo')[0];
+const $logo = document.querySelectorAll('.nav-bar')[0];
 window.addEventListener('scroll', togglelogo, false);
 function togglelogo(){
-    if (window.pageYOffset >= 160 && $logo.classList.contains('abs-pos')){
+    if (window.pageYOffset >= 160 ){
         $logo.classList.remove('abs-pos');
         $logo.classList.add('fix-pos');
-    } else if (window.pageYOffset < 160 && $logo.classList.contains('fix-pos')) {
+    } else if (window.pageYOffset < 160 ) {
         $logo.classList.add('abs-pos');
         $logo.classList.remove('fix-pos');
     }
